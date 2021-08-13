a284872b319ea3fe99ac4473261aa10848830686
Neosoft <dikshita.poojari@neosoftmail.com>
2021-08-13 17:57:10 +0530

    Third commit

---
+++ games/character/enemy.py
@@ -0,0 +1,2 @@
+def get_enemy_info():
+    print("I'm the enemy player")
---
+++ main.py
@@ -0,0 +1,5 @@
+from games.character import player
+from games.character.enemy import get_enemy_info
+
+player.get_player_info()
+get_enemy_info()
